c43a49029f33d4362f1edc150c0fd72fdd0cb2c2
sgavil <ssjsrgx@gmail.com>
2020-10-02 01:20:13 +0200

    Adding help section #3 and some parameters check

--- main.cpp
+++ main.cpp
@@ -1,101 +1,140 @@
 #include<iostream>
 #include<vector>
 #include<map>
+#include <functional>
+#include <sstream>
+
 using namespace std;
 
-enum child{
-    left, 
-    right
+enum child {
+	left,
+	right
 };
 
-void decode(string input){
-/*
-              Tree to find decoded output:
+//Available Commands
+const string ENCODE_COMMAND = "encode";
+const string DECODE_COMMAND = "decode";
+const string HELP_COMMAND = "--help";
 
-                          ROOT
-                    /              \
-   (left if . )    /                \   (right if - )
-                  /                  \
-                 /                    \
-                /                      \
-               E                        T
-             /   \                  /       \
-            /     \                /         \ 
-           /       \              /           \ 
-          I          A           N             M
-        /   \      /   \        /  \         /   \
-       S     U    R     W      D     K      G     O
-      / \   /    /     / \    / \   / \    / \
-     H   V F    L     P   J  B   X C   Y  Z   Q
+void decode(string input) {
+	/*
+				  Tree to find decoded output:
 
-*/
-    map<string, vector<string>> morseTree = {{"ROOT", {"E", "T"}}, {"E", {"I", "A"}}, {"T", {"N", "M"}}, {"I", {"S", "U"}}, {"A", {"R", "W"}}, {"N", {"D", "K"}}, {"M", {"G", "O"}}, {"S", {"H", "V"}}, {"U", {"F", "NIL"}}, {"R", {"L", "NIL"}}, {"W", {"P", "J"}}, {"D", {"B", "X"}}, {"K", {"C", "Y"}}, {"G", {"Z", "Q"}}};  
-    string output = "", cur = "ROOT";
-    int spcount = 0;
-    for(auto C: input){
-        if(C == ' '){
-            if(!spcount){ if(cur!="ROOT") output+=cur; spcount++; }
-            else        { output+=" "; spcount=0; }
-            cur="ROOT";
-        }else{
-            spcount = 0;
-            if(C == '.')cur = morseTree[cur][child::left];
-            else        cur = morseTree[cur][child::right];
-        }
-    }
-    if(cur!="ROOT") output+=cur;
-    cout<<"Decoded Text: "<<output<<endl;
+							  ROOT
+						/              \
+	   (left if . )    /                \   (right if - )
+					  /                  \
+					 /                    \
+					/                      \
+				   E                        T
+				 /   \                  /       \
+				/     \                /         \
+			   /       \              /           \
+			  I          A           N             M
+			/   \      /   \        /  \         /   \
+		   S     U    R     W      D     K      G     O
+		  / \   /    /     / \    / \   / \    / \
+		 H   V F    L     P   J  B   X C   Y  Z   Q
+
+	*/
+	map<string, vector<string>> morseTree = { {"ROOT", {"E", "T"}}, {"E", {"I", "A"}}, {"T", {"N", "M"}}, {"I", {"S", "U"}}, {"A", {"R", "W"}}, {"N", {"D", "K"}}, {"M", {"G", "O"}}, {"S", {"H", "V"}}, {"U", {"F", "NIL"}}, {"R", {"L", "NIL"}}, {"W", {"P", "J"}}, {"D", {"B", "X"}}, {"K", {"C", "Y"}}, {"G", {"Z", "Q"}} };
+	string output = "", cur = "ROOT";
+	int spcount = 0;
+	for (auto C : input) {
+		if (C == ' ') {
+			if (!spcount) { if (cur != "ROOT") output += cur; spcount++; }
+			else { output += " "; spcount = 0; }
+			cur = "ROOT";
+		}
+		else {
+			spcount = 0;
+			if (C == '.')cur = morseTree[cur][child::left];
+			else        cur = morseTree[cur][child::right];
+		}
+	}
+	if (cur != "ROOT") output += cur;
+	cout << "Decoded Text: " << output << endl;
 }
 
-void encode(string input){
-    map<char, string> dec = {
-        {'A', ".-"},
-        {'B', "-..."},
-        {'C', "-.-."},
-        {'D', "-.."},
-        {'E', "."},
-        {'F', "..-."},
-        {'G', "--."},
-        {'H', "...."},
-        {'I', ".."}, 
-        {'J', ".---"},
-        {'K', "-.-"},
-        {'L', ".-.."},
-        {'M', "--"},
-        {'N', "-."},
-        {'O', "---"},
-        {'P', ".--."},
-        {'Q', "--.-"},
-        {'R', ".-."},
-        {'S', "..."},
-        {'T', "-"},
-        {'U', "..-"},
-        {'V', "...-"},
-        {'W', ".--"},
-        {'X', "-..-"},
-        {'Y', "-.--"},
-        {'Z', "--.."}
-    };
-    string output="";
-    for(auto i: input){
-        if(i!=' '){
-            output+= dec[toupper(i)] + " ";
-        }else{
-            output+="  ";
-        }
-    }
-    cout<<"Encoded Text: "<<output<<endl;
+void encode(string input) {
+	map<char, string> dec = {
+		{'A', ".-"},
+		{'B', "-..."},
+		{'C', "-.-."},
+		{'D', "-.."},
+		{'E', "."},
+		{'F', "..-."},
+		{'G', "--."},
+		{'H', "...."},
+		{'I', ".."},
+		{'J', ".---"},
+		{'K', "-.-"},
+		{'L', ".-.."},
+		{'M', "--"},
+		{'N', "-."},
+		{'O', "---"},
+		{'P', ".--."},
+		{'Q', "--.-"},
+		{'R', ".-."},
+		{'S', "..."},
+		{'T', "-"},
+		{'U', "..-"},
+		{'V', "...-"},
+		{'W', ".--"},
+		{'X', "-..-"},
+		{'Y', "-.--"},
+		{'Z', "--.."}
+	};
+	string output = "";
+	for (auto i : input) {
+		if (i != ' ') {
+			output += dec[toupper(i)] + " ";
+		}
+		else {
+			output += "  ";
+		}
+	}
+	cout << "Encoded Text: " << output << endl;
 }
 
-int main(int argc, char **argv){
+void printHelp() {
 
-    if(argc<3){
-        cout<<"Too few arguments!\n";
-        exit(0);
-    }
-    string operation = argv[1], input = argv[2];
-    if(operation == "encode") encode(input);
-    else if(operation == "decode") decode(input);
-    else cout<<"Operation not identified! Aborting!";
-    return 0;
+	const string helpText(
+		"usage: morseconverter\t[decode <text>] Use one space to seperate letters and two spaces to seperate words.\n"
+		"\t\t\t[encode <text>] Only alphabet conversion supported. Example: encode 'HeY'\n"	
+	);
+
+	std::cout << helpText << endl;
+}
+
+//Map to bind commands to functions
+map<string, function<void(string)>>  funcMap =
+{ { ENCODE_COMMAND, encode},
+ { DECODE_COMMAND, decode}
+};
+
+int main(int argc, char** argv) {
+
+	if (argc > 1)
+	{
+		string operation = argv[1];
+
+		if (operation == ENCODE_COMMAND || operation == DECODE_COMMAND)
+		{
+			if (argc < 3)
+			{
+				cout << "Too few arguments!\n";
+				exit(0);
+			}
+			else {
+				string input = argv[2];
+				funcMap[operation](input);
+			}
+		}
+
+		else if (operation == HELP_COMMAND) printHelp();
+		else cout << "Operation not identified! Use --help for more information" << endl;
+	}
+	else cout << "Not command found, use --help for more information" << endl;
+	return 0;
 }
